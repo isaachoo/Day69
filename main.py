e63baccc8f37ca83c2de13ac657ba69895b8d581
Isaac <isaac@isaachoo.com>
2022-08-22 09:29:28 +0800

    9th commit. Use Postgre now.

--- main.py
+++ main.py
@@ -30,7 +30,8 @@ gravatar = Gravatar(app,
 
 ##CONNECT TO DB
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///blog.db")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL_SQL", "sqlite:///blog.db")
+print(os.environ.get("DATABASE_URL"))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
